9b013bba90315af27d81b29e866139bf03ba8d9a
paminus-kingori <pkmurungi.pm@gmail.com>
2020-05-26 06:55:04 +0300

    Added fill circles in carousel

--- manage.js
+++ manage.js
@@ -1,8 +1,10 @@
+// mobile menu
 const iconHolder = document.getElementById('icon-holder');
 const ham = document.querySelector('#icon-ham');
 const close = document.querySelector('#icon-close');
 const links = document.querySelector('#links');
 const overlay = document.getElementById('overlay');
+
 
 iconHolder.addEventListener('click',function(){
      ham.classList.toggle('none');
@@ -55,3 +57,95 @@ function instaFtn(){
 function removeInsta(){
      insta.src ="images/icon-instagram.svg";
 }
+
+//creating the slider
+const holder = document.querySelector('#person-holder');
+const people = document.querySelectorAll('.person');
+
+//circles
+const dots = document.querySelectorAll('.dot');
+let i =0;
+
+//buttons
+const next = document.querySelector('#nextBtn');
+const prev = document.querySelector('#prevBtn');
+
+counter = 1;
+const size = people[0].clientWidth;
+
+slider(counter);
+circleFill();
+
+function slider(x){
+     holder.style.transform = 'translateX('+ (-size * x) +'px)';
+}
+function moveForward(){
+     if(counter>=people.length-1) return;
+     console.log('mbele');
+     holder.style.transition = "transform 0.4s ease-in-out";
+     counter++;
+     slider(counter);
+     circleFill();
+    remNext();
+}
+function moveBackward(){
+     if(counter<=0) return;
+
+     holder.style.transition = "transform 0.4s ease-in-out";
+     counter--;
+     slider(counter);
+     circleFill();
+     remPrev();
+}
+
+// making the loop
+holder.addEventListener('transitionend',function(){
+if(people[counter].id==='CloneLast'){
+     holder.style.transition = 'none';
+     counter = people.length - 2;
+     slider(counter);
+     circleFill();
+         dots[0].classList.remove('background');
+}
+if(people[counter].id==='CloneFirst'){
+     holder.style.transition = 'none';
+     counter = people.length - counter;
+     slider(counter);
+     circleFill();
+     dots[3].classList.remove('background');
+}
+});
+
+
+     
+     function circleFill(){
+         if(counter==1){
+             i = 0;
+         }
+         if(counter==2){
+             i = 1;
+         }
+         if(counter==3){
+             i=2;
+         }
+         if(counter==4){
+             i=3;
+         }
+         if(counter==5){
+             i = 4;
+         }
+         console.log(i);
+
+         dots[i].classList.add('background');
+         
+     }
+     function remNext(){
+         dots[i-1].classList.remove('background');
+     }
+     function remPrev(){
+         dots[i+1].classList.remove('background');
+     }
+     
+
+next.addEventListener('click',moveForward);
+prev.addEventListener('click',moveBackward);
